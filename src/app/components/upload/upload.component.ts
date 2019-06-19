6a8e8027a7579a8f01bb73f4e3a935f43a7af3d9
anushka1229 <anushka.srivastava7@gmail.com>
2019-06-20 00:24:35 +0530

    upload component with reactive forms
    upload component

--- src/app/components/upload/upload.component.ts
+++ src/app/components/upload/upload.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, FormArray, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadComponent implements OnInit {
 
-  constructor() { }
+ public uploadForm:any;
+ public teamMembers: Array<string>;
+ public contributors: Array<string>;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-  }
+    this.uploadForm = this.fb.group({
+      projectName: new FormControl(''),
+      authorId: new FormControl(''),
+      csiId:new FormControl(''),
+      technology:new FormControl(''),
+      background:this.fb.group({
+        objective:new FormControl(''),
+        deliverables: new FormControl(''),
+        constraints: new FormControl('')
+      }),
+     
+       roles: this.fb.group({
+         manager:new FormControl(''),
+        members: this.fb.array([this.teamMembers]),
+         keyStakeholder: new FormControl('')
+       }),
 
+       path: new FormControl(''),
+       contributors: this.fb.array([this.contributors])
+
+
+    });
+  }
+ 
 }
